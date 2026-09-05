56fc4c103e2645f64813188c48784a3d9c383cd7
Rajesh <104371585+rajeshgudi@users.noreply.github.com>
2026-09-05 11:21:41 +0530

    Updated app.js file

---
+++ app.js
@@ -0,0 +1,8 @@
+This is the javascript code.
+  // Save this file as HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println prints text to the console with a new line
+        System.out.println("Hello, World!");
+    }
+}
